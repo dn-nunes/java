ad15f13c126aee8397cb4ce6eeb30de8b9cc30ac
Daniel Nunes <dev.danielnunes@gmail.com>
2026-01-11 23:28:44 -0300

    Created basic options logic. #002

--- Finanva/src/Main.java
+++ Finanva/src/Main.java
@@ -5,26 +5,31 @@ public class Main {
     public static void main() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Nome do proprietário: ");
-        String name = scanner.nextLine();
+        // System.out.print("Nome do proprietário: ");
+        // String name = scanner.nextLine();
+        String name = "Daniel";
 
-        System.out.print("Tipo de conta: ");
-        String accountType = scanner.nextLine();
+        // System.out.print("Tipo de conta: ");
+        // String accountType = scanner.nextLine();
+        String accountType = "Conta Corrente";
 
-        System.out.print("Saldo inicial: ");
-        double openingBalance = scanner.nextDouble();
+        // System.out.print("Saldo inicial: ");
+        // double openingBalance = scanner.nextDouble();
+        double openingBalance = 2500.50;
+
 
         System.out.printf("""
-                
-                === Dados da Conta ===
-                
-                Nome do proprietário: %s
-                Tipo de conta: %s
-                Saldo inicial: R$%.2f
-                
-                """, name, accountType, openingBalance);
+            
+            === Dados da Conta ===
+            
+            Nome do proprietário: %s
+            Tipo de conta: %s
+            Saldo inicial: R$%.2f
+            
+            """, name, accountType, openingBalance);
 
-        System.out.println("""
+        while (true) {
+            System.out.println("""
                 Operações:
                 
                 1 - Consultar saldo atual
@@ -33,7 +38,40 @@ public class Main {
                 0 - Sair
                 """);
 
-        System.out.print("Digite a operação desejada: ");
-        int option = scanner.nextInt();
+            System.out.print("Digite a operação desejada: ");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.printf("""
+                        
+                        === Saldo atual da conta ===
+                        
+                        Saldo: %.2f
+                        
+                        """, openingBalance);
+                    break;
+                case 2:
+                    System.out.print("\n=== Recebimento ===\n");
+                    System.out.print("Valor a receber: ");
+
+
+                    double receiveValue = scanner.nextDouble();
+                    openingBalance += receiveValue;
+                    break;
+                case 3:
+                    System.out.print("\n=== Transferência ===\n");
+                    System.out.print("Valor a transferir: ");
+
+                    double transferValue = scanner.nextDouble();
+                    openingBalance -= transferValue;
+                    break;
+                case 0:
+                    System.out.println("Fechando a aplicação...");
+                    return;
+                default:
+                    break;
+            }
+        }
     }
 }
